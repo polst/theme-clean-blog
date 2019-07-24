2c51d30a5fbf02cab6b0b914b26178aaa1a51fa3
basic-app <dev@basic-app.com>
2019-07-24 22:36:35 +0300

    n/a

--- Theme.php
+++ Theme.php
@@ -4,4 +4,12 @@ namespace BasicApp\CleanBlogTheme;
 
 class Theme extends ThemeAbstract
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->head .= Html::linkCss('/themes/startbootstrap-clean-blog/custom.css'); 
+    }
+
 }
