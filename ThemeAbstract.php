1c123e476aa4b44f055cbf473c363c1f22034a37
basic-app <dev@basic-app.com>
2019-10-21 16:02:05 +0300

    n/a

--- ThemeAbstract.php
+++ ThemeAbstract.php
@@ -2,7 +2,7 @@
 
 namespace BasicApp\CleanBlogTheme;
 
-abstract class ThemeAbstract extends \PhpTheme\CleanBlogTheme\Theme
+abstract class ThemeAbstract extends \PhpTheme\CleanBlogTheme\Theme implements \BasicApp\System\ThemeInterface
 {
 
     public $baseUrl = '/themes/startbootstrap-clean-blog';
