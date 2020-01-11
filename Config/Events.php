8b1710ad393f4825f329b0262978063438171d41
basic-app <dev@basic-app.com>
2020-01-11 23:00:53 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -3,7 +3,7 @@
 use BasicApp\Site\SiteEvents;
 use BasicApp\System\SystemEvents;
 use BasicApp\Helpers\CliHelper;
-use BasicApp\CleanBlogTheme\Theme
+use BasicApp\CleanBlogTheme\Theme;
 
 if (class_exists(SystemEvents::class))
 {
